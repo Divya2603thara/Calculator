7dd19a89ec366ea6be295907e6b7e37527ca1875
Divya2603thara <117896861+Divya2603thara@users.noreply.github.com>
2023-03-30 16:40:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// function create_html_tag()
+// {
+//     content=document.getElementById("input_text").value; //taking inputtexts
+//     para =document.createElement("p");  //creating p tag'
+
+//     data=document.createTextNode(content); //typing data in content
+
+//     para.appendChild(data); //add
+
+//     mydiv=document.getElementById("div1") //taking div
+//     mydiv.appendChild(para); //adding para to div
+
+
+// }
